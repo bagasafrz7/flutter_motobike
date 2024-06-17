02b56ad55f42c4f7d8f9c6f62371035265202430
bagasafrz7 <bagasafrz16@gmail.com>
2024-06-17 17:42:39 +0700

    feat: slicing SplashScreen and Sign Up & Create widgets

--- lib/pages/signup_pages.dart
+++ lib/pages/signup_pages.dart
@@ -1,4 +1,9 @@
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_motobike/widgets/button_primary.dart';
+import 'package:flutter_motobike/widgets/button_secondary.dart';
+import 'package:flutter_motobike/widgets/input.dart';
+import 'package:gap/gap.dart';
 
 class SignUpPages extends StatefulWidget {
   const SignUpPages({super.key});
@@ -8,8 +13,79 @@ class SignUpPages extends StatefulWidget {
 }
 
 class _SignUpPagesState extends State<SignUpPages> {
+  final edtName = TextEditingController();
+  final edtEmail = TextEditingController();
+  final edtPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 0
+        ),
+        children: [
+          const Gap(100),
+          Image.asset(
+            'assets/logo_text.png',
+            height: 38,
+            width: 171,
+          ),
+          const Gap(70),
+          const Text(
+            'Sign Up Account',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              color: Color(0xff070623)
+            ),
+          ),
+          const Gap(30),
+          const Text(
+            'Complete Name',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Color(0xff070623)
+            ),
+          ),
+          const Gap(12),
+          Input(icon: 'assets/ic_profile.png', hint: 'Write your real name', editingController: edtName),
+          const Gap(20),
+          const Text(
+            'Email Address',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Color(0xff070623)
+            ),
+          ),
+          const Gap(12),
+          Input(icon: 'assets/ic_email.png', hint: 'Write your real email', editingController: edtEmail),
+          const Gap(20),
+          const Text(
+            'Password',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Color(0xff070623)
+            ),
+          ),
+          const Gap(12),
+          Input(icon: 'assets/ic_key.png', hint: 'Write your password', editingController: edtPassword, obsecure: true),
+          const Gap(30),
+          ButtonPrimary(text: 'Create New Account', onTap: () {}),
+          const Gap(30),
+          const DottedLine(
+            dashLength: 6,
+            dashGapLength: 6,
+            dashColor: Color(0xffCECED5),
+          ),
+          const Gap(30),
+          ButtonSecondary(text: 'Sign In', onTap: () {}),
+          const Gap(30),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_motobike/widgets/button_primary.dart';
+import 'package:gap/gap.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +13,63 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var padding = const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 24
+            ),
+            child: Text(
+              'We provide all beautiful motorbike for your road trip and great memories of life.!',
+              style: TextStyle(
+                height: 1.7,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff070623)
+              ),
+            ),
+          );
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Gap(70),
+          Image.asset(
+            'assets/logo_text.png',
+            height: 38,
+            width: 171,
+          ),
+          const Gap(10),
+          const Text(
+            'Drive & Be Happy!',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              color: Color(0xff070623)
+            ),
+          ),
+          Expanded(
+            child: Transform.translate(
+              offset: const Offset(-99, 0),
+              child: Image.asset(
+                'assets/splash_screen.png',
+              ),
+            ),
+          ),
+          padding,
+          const Gap(30),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24
+            ),
+            child: ButtonPrimary(
+              text: 'Explore Now',
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/signup');
+              },
+            ),
+          ),
+          const Gap(50),
+        ],
+      ),
+    );
   }
 }
